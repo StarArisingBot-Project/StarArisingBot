2b1f6661f9ee9ae6bb8e023066a9f263b6c36530
Starciad <69594923+Starciad@users.noreply.github.com>
2022-07-28 15:55:37 -0300

    ✨​ FEAT ➜ 『 Library addition 』 »——————⋆◦★◦⋆——————«
    ● Adding and creating the StarArisingBotFramework library.
    
    »——————⋆◦★◦⋆——————«

---
+++ StarArisingBotFramework/Attributes/Commands/CommandCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarArisingBotFramework.Attributes.Commands
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class CommandCategory : Attribute
+    {
+        public string Name { get; }
+
+        public CommandCategory(string name)
+        {
+            Name = name;
+        }
+    }
+}
