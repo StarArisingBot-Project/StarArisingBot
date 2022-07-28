978c93bd2eb57388f01746abd792bf3c1d5d3b59
Starciad <69594923+Starciad@users.noreply.github.com>
2022-07-27 21:57:43 -0300

    ✨ FEAT ➜『 Project Restructuring 』 »——————⋆◦★◦⋆——————«
    ● The DotEnv.cs & SABBotTokens.cs files are now located in the System folder.
    
    »——————⋆◦★◦⋆——————«

---
+++ StarArisingBot/System/Bot/SABBotTokens.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarArisingBot.Business.System
+{
+    public class SABBotTokens
+    {
+        public string BotToken { get; set; }
+    }
+}
